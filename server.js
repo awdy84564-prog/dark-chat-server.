c16c31189d43b250e2772a0cdae2bbf0c3acac02
awdy84564-prog <awdy84564@gmail.com>
2026-06-06 17:59:46 +0300

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: { origin: "*" }
+});
 
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+  console.log('user connected:', socket.id);
+  
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  
+  socket.on('mic', (data) => {
+    io.emit('mic', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
